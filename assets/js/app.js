8be0fe9a738f273402817c0c6a6f75954f0a6cc2
caiquefgoncalves123 <caique.f.goncalves@aluno.senai.br>
2025-09-24 15:57:24 -0300

    Finalização das partes DESKTOP e inicio do responsivo
    Foram finalizadas a parte de Desktop e iniciei o responsivo, foi adicionado o menu hamburguer para a versão mobile, que estamos iniciando primeiro e finalizei o mobile da home

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+const btnMenu = document.getElementById('btnMenu');
+const menuMobile = document.getElementById('menuMobile');
+const btnFechar = document.getElementById('btnFechar');
+const header = document.querySelector('.cabecalho-principal');
+
+btnMenu.addEventListener('click', function() {
+    menuMobile.classList.add('aberto');
+    header.classList.add('menu-aberto');
+});
+
+btnFechar.addEventListener('click', function() {
+    menuMobile.classList.remove('aberto');
+    header.classList.remove('menu-aberto');
+});
+
+
+document.querySelectorAll('#menuMobile a').forEach(link => {
+    link.addEventListener('click', function() {
+        menuMobile.classList.remove('aberto');
+        header.classList.remove('menu-aberto');
+    });
+});
